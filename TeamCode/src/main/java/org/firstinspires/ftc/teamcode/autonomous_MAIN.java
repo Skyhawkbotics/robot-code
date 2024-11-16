0a66c23211f40f4a9b409fa879ef81dc19bb25fa
trevor <trevor271c@protonmail.com>
2024-11-15 23:19:16 -0800

    bunch o changes it works better now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous_MAIN.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous_MAIN.java
@@ -22,6 +22,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.acmerobotics.roadrunner.*;
@@ -35,17 +36,22 @@ public class autonomous_MAIN extends LinearOpMode {
     public class Elevator {
         private TouchSensor up_zero;
         private DcMotorEx up;
-
+        private DcMotorEx out;
+        //private Servo servo_CLAW;
         public Elevator(HardwareMap hardwareMap) {
             up_zero = hardwareMap.get(TouchSensor.class, "up_zero");
             up = hardwareMap.get(DcMotorEx.class, "up");
+            out = hardwareMap.get(DcMotorEx.class, "out");
+            //servo_CLAW = hardwareMap.get(Servo.class, "claw");
         }
 
+        //sets everything to home position
         public class CalibrateDown implements Action {
             private boolean initialized = false;
 
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
+                //zero out "up" using button "up_zero"
                 if (!initialized) {
                     up.setVelocity(-500);
                     initialized = true;
@@ -55,39 +61,102 @@ public class autonomous_MAIN extends LinearOpMode {
                     return true;
                 } else {
                     up.setVelocity(0);
+
+                    //zero out "claw" servo
+                    //servo_CLAW.setPosition(0);
+
+                    //zero out "out"
+                    out.setTargetPosition(-1008);
+                    out.setVelocity(400);
+                    out.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                     return false;
                 }
+
             }
         }
 
-        /*public class ElevatorMove implements Action {
+//moves up to specified position
+        public class ElevatorMove implements Action {
 
             private int pos = 0;
-            public ElevatorMove(int pos_input) {
+            private String motor = "";
+            private int true_target_pos = 0;
+
+            public ElevatorMove(int pos_input, String motor_input) {
+                pos = pos_input;
+                motor = motor_input;
+            }
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                //movement using velocity mode to a certain position pos
+                if (motor == "up") {
+                    if (up.getCurrentPosition() < pos) {
+                        true_target_pos = 0;
+                        up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                        up.setVelocity(300);
+                    } else if (up.getCurrentPosition() > pos) {
+                        true_target_pos = 0;
+                        up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                        up.setVelocity(-300);
+                    }  else {
+                        if (true_target_pos == 0) {
+                            up.setTargetPosition(up.getCurrentPosition());
+                            true_target_pos = up.getCurrentPosition();
+                        }
+                        up.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        return false;
+                    }
+                    return false;
+                } else if (motor == "out") {
+                    if (out.getCurrentPosition() < pos) {
+                        true_target_pos = 0;
+                        out.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                        out.setVelocity(300);
+                    } else if (up.getCurrentPosition() > pos) {
+                        true_target_pos = 0;
+                        out.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                        out.setVelocity(-300);
+                    }  else {
+                        if (true_target_pos == 0) {
+                            out.setTargetPosition(out.getCurrentPosition());
+                            true_target_pos = out.getCurrentPosition();
+                        }
+                        out.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                        return false;
+                    }
+                    return false;
+                }
+                return false;
+
+
+            }
+
+        }
+
+        public class ClawMove implements Action {
+            private double pos;
+            public ClawMove(double pos_input) {
                 pos = pos_input;
             }
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
-                if (up.getCurrentPosition() < pos) {
-                    up_true_target_pos = 0;
-                } else (up.getCurrentPosition() > pos) {
-                    up.setVelocity();
-                    up_true_target_pos = 0;
-                }  else {
-                    up.setTargetPosition(up.getCurrentPosition());
-                    up_true_target_pos = up.getCurrentPosition();
-                }
+                //servo_CLAW.setPosition(pos);
+                return false;
             }
 
-        }*/
+        }
 
         public Action calibrateDown() {
             return new CalibrateDown();
         }
 
-        /*public Action elevatorMove(int pos) {
-            return new  ElevatorMove(pos);
-        }*/
+        public Action elevatorMove(int pos, String motor) {
+            return new  ElevatorMove(pos, motor);
+        }
+        public Action clawMove(double pos) {
+            return new ClawMove(pos);
+        }
     }
 
     private DcMotorEx up; //name of motor is up(in the code);
@@ -136,10 +205,10 @@ public class autonomous_MAIN extends LinearOpMode {
         TrajectoryActionBuilder leftCorner = drive.actionBuilder(/*start position*/new Pose2d(0.0, 5.0, 0.0)) // tells the robot where it's going to start?
 
         //directions from start postion
-            .strafeTo(new Vector2d(0.0, 10.0)) // went to the left 10 units(inches?)
-            .waitSeconds(3.0) //waits for 3 seconds
+            .strafeTo(new Vector2d(0.0, 10.0)); // went to the left 10 units(inches?)
+            //.waitSeconds(3.0) //waits for 3 seconds
             //.lineToX(10.0) //foward 10 units, seems useless as it kind of gets crooked, and not the right angle
-            .strafeTo(new Vector2d(10, 15));
+            //.strafeTo(new Vector2d(10, 15));
             //.turn(2) //turns way further than 2 radians, who knows why. real
             //    .setTangent(Math.toRadians(180));
 
@@ -156,10 +225,15 @@ public class autonomous_MAIN extends LinearOpMode {
         //run the action leftCorner, first building it to make it runnable as a action, as a result we can no longer edit this action!
         Action leftCornerBuild;
         leftCornerBuild = leftCorner.build();
+
         Actions.runBlocking(
-                new ParallelAction(
-                        leftCornerBuild,
+                new SequentialAction(
                         elevator.calibrateDown()
+                        //new ParallelAction(
+                                //leftCornerBuild,
+                                //elevator.elevatorMove(200, "out"),
+                                //elevator.elevatorMove(100, "up")
+                        //)
                 )
         );
 
