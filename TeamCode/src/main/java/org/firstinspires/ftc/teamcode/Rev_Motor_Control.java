2b44ff203a9a27e779f1aca89c9015bdb5ec94ca
loduduma <loduduma@gmail.com>
2024-12-03 16:27:02 -0800

    Spool Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rev_Motor_Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rev_Motor_Control.java
@@ -7,12 +7,39 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareDeviceCloseOnTearDown;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 @TeleOp(name="Rev_Motor_Control")
 public class Rev_Motor_Control extends LinearOpMode {
-
+// Front is on expansion hub port 3
+    // viper slide is on expansion hub port 2
     // Declare motor and game pad
-    private DcMotorEx revMotor;  // Rev motor to control
+    private DcMotorEx revMotor;
+    private CRServo servo_Intake; // Rev motor to control
+
+    private CRServo servo_Outtake;
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry()); // sets up tel em
@@ -22,6 +49,10 @@ public class Rev_Motor_Control extends LinearOpMode {
         revMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         revMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         revMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        servo_Outtake = hardwareMap.get(CRServo.class, "claw");
+        servo_Intake = hardwareMap.get(CRServo.class, "claw2");
+
 
 
         // Wait for the game to start
@@ -42,6 +73,23 @@ public class Rev_Motor_Control extends LinearOpMode {
                 revMotor.setVelocity(0);
                 telemetry.addData("Nothing pressed", true);
             }
+            // servo one
+            if (gamepad2.right_trigger > 0.8/* && servo_CLAW_position < 1000000000*/) {
+                    servo_Outtake.setPower(1);
+            } else if (gamepad2.right_bumper/* && servo_CLAW_position > -100000000*/) {
+                    servo_Outtake.setPower(-1);
+            } else {
+                    servo_Outtake.setPower(0);
+            }
+
+            if (gamepad2.left_trigger > 0.8) {
+                servo_Intake.setPower(1);
+            } else if (gamepad2.left_bumper/* && servo_CLAW_position > -100000000*/) { //TODO: these limits too.
+                servo_Intake.setPower(-1);
+            } else {
+                servo_Intake.setPower(0);
+            }
+
             // Telemetry to display motor status on the driver station
             //telemetry stuff (prints stuff on the telemetry (driver hub))
             telemetry.addData("armCurrentPosition", revMotor.getCurrentPosition());
