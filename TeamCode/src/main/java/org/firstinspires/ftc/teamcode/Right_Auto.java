6dc1d08998050d3ff3fea748090095629fd4aee0
duduma <loduduma@gmail.com>
2024-12-12 19:44:55 -0800

    SOMEONE FIX THIS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Right_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Right_Auto.java
@@ -198,9 +198,9 @@ public class Right_Auto extends LinearOpMode { // extends means inherits from li
         Actions.runBlocking(
                 new SequentialAction(
                         trajectoryActionChosen,
-                        elevator.elevator_Up_Move(),
-                        elevator_claw.elevator_Claw_Move(),
-                        elevator.elevator_Down_Move(),
+                        elevator_up_move(),
+                        elevator_claw_move(),
+                        elevator_down_move(),
                         trajectoryActionCloseOut
                 )
         );
