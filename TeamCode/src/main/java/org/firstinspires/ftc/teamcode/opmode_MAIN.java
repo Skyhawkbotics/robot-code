796300d3c94d7db5e4fa4a1e27c03f9c5f2a782e
trevor <trevor271c@protonmail.com>
2024-12-14 11:05:17 -0800

    updated code, from eric because his git has some issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode_MAIN.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode_MAIN.java
@@ -50,7 +50,7 @@ public class opmode_MAIN extends LinearOpMode {
     int transfer_step = 0;
     double intake_wrist_pos_transfer = 0.1;
     double outtake_wrist_pos_transfer = 0.2;
-    int out_pos_transfer = 30;//TODO: edit this for calibration!
+    double out_pos_transfer = 30;
     int up_pos_transfer1 = 300;
     double up_pos_transfer2 = 10;
     double up_pos_transfer3 = 20;
@@ -66,25 +66,25 @@ public class opmode_MAIN extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        
 
-            //setup arm to use velocity
-            up = hardwareMap.get(DcMotorEx.class, "up");
-            up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            up.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            //example position setup
-            out = hardwareMap.get(DcMotorEx.class, "out");
-            out.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            out.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            out.setDirection(DcMotorSimple.Direction.REVERSE);
+        //setup arm to use velocity
+        up = hardwareMap.get(DcMotorEx.class, "up");
+        up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        up.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            //example velocity setup
-            //up = hardwareMap.get(DcMotorEx.class, "up");
-            //up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            //up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            //up.setDirection(DcMotorSimple.Direction.REVERSE);
+        //example position setup
+        out = hardwareMap.get(DcMotorEx.class, "out");
+        out.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        out.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        out.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //example velocity setup
+        //up = hardwareMap.get(DcMotorEx.class, "up");
+        //up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //up.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //setup servos for intake and outtake
         servo_intake = hardwareMap.get(CRServo.class, "intake");
@@ -155,13 +155,12 @@ public class opmode_MAIN extends LinearOpMode {
                     out.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                     out.setVelocity(gamepad2.right_stick_y * 500);
                     out_true_target_pos = 0;
-                } else if (out_zero.isPressed() && gamepad2.right_stick_y > 0.1) { // Lower limit for up
-                telemetry.addData("Lower Limit Reached", out_zero);
-                up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                } else { // This is newly commented out code, this code kinda of functions like stay-upper? ; it kinda make sures the motor keeps same position
+                } else if (out_zero.isPressed()) {
+                    out.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                }else { // This is newly commented out code, this code kinda of functions like stay-upper? ; it kinda make sures the motor keeps same position
                     // I thought this wasn't needed for misumi since theres no gravity unlike the viper slide. I also commented it out so I could set a run to target posotion
                     // without possibly interfering with the misumi slide controls. If i am wrong just uncomment the code(He was wrong, it was necessary)
-                   out.setPower(500);
+                    out.setPower(500);
                     //use position mode to stay up, as otherwise it would fall down. do some fancy stuff with up_true_target_pos to avoid the issue of it very slightly falling every tick
                     if (out_true_target_pos == 0) {
                         out.setTargetPosition(out.getCurrentPosition());
@@ -169,14 +168,15 @@ public class opmode_MAIN extends LinearOpMode {
                     }
                     out.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 }
+            }
 
 
-                //make sure the upper and lower limits are actually at the upper and lower limits
-                if (up.getCurrentPosition() < 0) {
-                    up.setTargetPosition(0);
-                } else if (up.getCurrentPosition() > arm_upper_lim) {
-                    up.setTargetPosition(arm_upper_lim);
-                }
+            //make sure the upper and lower limits are actually at the upper and lower limits
+            if (up.getCurrentPosition() < 0) {
+                up.setTargetPosition(0);
+            } else if (up.getCurrentPosition() > arm_upper_lim) {
+                up.setTargetPosition(arm_upper_lim);
+            }
 
 
 
@@ -241,7 +241,6 @@ public class opmode_MAIN extends LinearOpMode {
                 if (gamepad2.b) {
                     //Add a variable and thing for setting the viper slide position to about 250 to avoid smashing stuff together
                     up.setTargetPosition(up_pos_transfer1);
-                    out.setTargetPosition(out_pos_transfer);
                     servo_intake_wrist_location = intake_wrist_pos_transfer;
                     servo_outtake_wrist_location = outtake_wrist_pos_transfer;
                 }
